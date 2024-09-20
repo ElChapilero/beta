c2a751962ceeec05deab17654e4cbb4a43c05dc0
Chapilero <xchapilerox@gmail.com>
2024-09-20 11:48:12 -0500

    coneccion en clase

--- api/config/db_connection.php
+++ api/config/db_connection.php
@@ -8,4 +8,15 @@
     $password = "unicesmag";
     $dbname = "beta";
     $port = "5432";
+
+    $data_connections = "
+    host =$host
+    port =$port
+    dbname =$dbname
+    user =$username
+    password =$password
+    ";
+    
+    // Create connection
+    $conn = pg_connect($data_connections)
 ?>
