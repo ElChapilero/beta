34d7f37aeb5150d3e6089fb6765b5ab925968e41
Chapilero <xchapilerox@gmail.com>
2024-09-20 11:37:08 -0500

    coneccion en clase

---
+++ api/config/db_connection.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+    Data Base connection
+    DeverLaper: Chapi
+    */
+    $host = "localhost"; //127.0.0.1
+    $username = "postgres";
+    $password = "unicesmag";
+    $dbname = "beta";
+    $port = "5432";
+?>
